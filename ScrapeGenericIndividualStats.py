bc794bd24ec8e6e43b95a0179600ffb0c97f41fb
Jenn Leonard <jennifer.m.leonard@gmail.com>
2014-06-29 11:46:18 -0700

    adding py files

---
+++ ScrapeGenericIndividualStats.py
@@ -0,0 +1,53 @@
+import urllib2
+import re
+from bs4 import BeautifulSoup
+import sys
+import codecs
+import psycopg2
+
+#url = 'http://www.procyclingstats.com/rankings/UCI_Individual_2014_06_02_EuropeTour'
+if len(sys.argv) < 2:
+	print "please provide a url"
+else:
+	# connect to db
+	try:
+    		conn = psycopg2.connect("dbname='cycling_stats' user='postgres' host='localhost' password='")
+	except:
+    		print "unable to connect to the database"
+	else:		
+
+	    if (conn != None):
+	    	dbCursor = conn.cursor();
+
+		# get data from website
+		url = sys.argv[1]
+		tableName = sys.argv[2]
+		page = urllib2.urlopen(url).read()
+		soup = BeautifulSoup(page)
+		table = soup.table['id']
+
+		UTF8Writer = codecs.getwriter('utf8')
+		sys.stdout = UTF8Writer(sys.stdout)
+
+		statsTable = soup.find("table", { "id": "list9"})
+		#f = codecs.open(sys.argv[2],'w',encoding='utf8')
+		for tr in statsTable.find_all('tr')[1:]:
+		 	tds = tr.find_all('td')
+		 	string  = re.match(r'^(\d*).(\d{3,6})', tds[5].text.strip())
+		 	if (string != None) :
+		 		formatted = re.sub('\.', '', tds[5].text.strip())
+		 	else :
+		 		formatted = tds[5].text.strip()
+
+		 	if ((table != None) & (dbCursor != None)) :
+		 		strValues = tds[0].text[:-1] + ",'" + tds[3].text.strip() + "','" +  tds[4].text.strip() + "'," +  formatted + ', current_timestamp'
+		 		strQuery = """INSERT INTO """ + tableName.strip() + """(Rank, Name, Team, Points, Timestamp) VALUES (""" + strValues + """)"""
+		 		try:
+	    				dbCursor.execute(strQuery)
+				except Exception as e:
+	    				print "cannot insert into database! " + strQuery
+		 			print tds[0].text[:-1] + '\t' + tds[3].text.strip() + '\t' +  tds[4].text.strip() + '\t' +  formatted
+			
+		conn.commit()
+		dbCursor.close()
+		conn.close()	
